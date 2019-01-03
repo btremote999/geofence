fbe07b41445b331bc687e9e177136dc464741bc1
Kok Keong Low <btremote999@gmail.com>
2019-01-03 23:42:39 +0800

    setup google map

--- app/src/main/java/btremote999/geofencetest/MainActivity.java
+++ app/src/main/java/btremote999/geofencetest/MainActivity.java
@@ -5,27 +5,41 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import btremote999.geofencetest.utils.Logger;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private static final String TAG = "MainActivity";
+    private GoogleMap mMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
+
+        // setup  google map
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        if(mapFragment != null)
+            mapFragment.getMapAsync(this);
+
+
     }
 
     @Override
@@ -49,4 +63,17 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Logger.d(TAG, "onMapReady: ");
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+
 }
--- app/src/main/java/btremote999/geofencetest/utils/Logger.java
+++ app/src/main/java/btremote999/geofencetest/utils/Logger.java
@@ -1,7 +1,8 @@
-package pollyfun.tvboxlib.util;
+package btremote999.geofencetest.utils;
 
 import android.util.Log;
-import com.crashlytics.android.Crashlytics;
+
+//import com.crashlytics.android.Crashlytics;
 
 /**
  * Created by kklow on 7/1/17.
@@ -9,95 +10,46 @@ import com.crashlytics.android.Crashlytics;
 
 public class Logger {
 
-    public interface IInterceptor {
-        void onLog(int logLevel, String tag, String msg);
-    }
-
-    private static IInterceptor sInterceptor = null;
-    public static void setInterceptor(IInterceptor interceptor){
-        sInterceptor = interceptor;
-    }
-
     // region standard debug log
     public static void d(String tag, String format, Object... args) {
         try {
             String msg = String.format(format, args);
-            Crashlytics.log(Log.DEBUG, tag, msg);
-            intercept(Log.DEBUG, tag, msg);
+            Log.d(tag, msg);
         } catch (Exception e) {
-            Crashlytics.log(Log.ERROR, tag, "Exception during create log. " + e.getMessage());
             e.printStackTrace();
         }
     }
 
-    private static void intercept(int logLevel, String tag, String msg) {
-        if(sInterceptor != null)
-            sInterceptor.onLog(logLevel, tag, msg);
 
-    }
 
     public static void i(String tag, String format, Object... args) {
         try {
             String msg = String.format(format, args);
-            Crashlytics.log(Log.INFO, tag, msg);
-            intercept(Log.INFO, tag, msg);
-
+            Log.i(tag, msg);
         } catch (Exception e) {
-            Crashlytics.log(Log.ERROR, tag, "Exception during create log. " + e.getMessage());
             e.printStackTrace();
         }
     }
 
     public static void w(String tag, String format, Object... args) {
         try {
-            int priority = Log.WARN;
             String msg = String.format(format, args);
-            Crashlytics.log(priority, tag, msg);
-            intercept(priority, tag, msg);
-//            Log.w(tag, String.format(format, args));
+            Log.w(tag, msg);
         } catch (Exception e) {
-            Crashlytics.log(Log.ERROR, tag, "Exception during create log. " + e.getMessage());
             e.printStackTrace();
         }
     }
 
     public static void e(String tag, String format, Object... args) {
         try {
-            int priority = Log.ERROR;
             String msg = String.format(format, args);
-            Crashlytics.log(priority, tag, msg);
-            intercept(priority, tag, msg);
+            Log.e(tag, msg);
         } catch (Exception e) {
-            Crashlytics.log(Log.ERROR, tag, "Exception during create log. " + e.getMessage());
             e.printStackTrace();
         }
     }
     //endregion standard debug log
 
-    public static void logException(Exception exception) {
-        Crashlytics.logException(exception);
-        int priority = Log.ASSERT;
-        String msg = exception.getMessage();
-        intercept(priority, "Exception", msg);
 
-        exception.printStackTrace();
-    }
-    public static String logLvlToString(int level){
-        switch(level) {
-            case Log.DEBUG:
-                return "D";
-            case Log.INFO:
-                return "I";
-            case Log.WARN:
-                return "W";
-            case Log.ERROR:
-                return "E";
-            case Log.ASSERT:
-                return "A";
-            default:
-                return "U";
-        }
-
-    }
 
 }
